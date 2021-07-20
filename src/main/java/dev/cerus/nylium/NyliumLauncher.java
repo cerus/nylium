0fe23457a7468173046c77feb7214bae28bc3e63
Cerus <m.dorn.2003@web.de>
2021-07-20 20:06:11 +0200

    Start basic server

--- src/main/java/dev/cerus/nylium/NyliumLauncher.java
+++ src/main/java/dev/cerus/nylium/NyliumLauncher.java
@@ -1,8 +1,66 @@
 package dev.cerus.nylium;
 
+import dev.cerus.nylium.event.EventBus;
+import dev.cerus.nylium.io.NettyBootstrapper;
+import dev.cerus.nylium.io.session.PlayerSessionController;
+import dev.cerus.nylium.server.listener.LoginListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 public class NyliumLauncher {
 
+    private static final Logger LOGGER = Logger.getLogger("");
+
     public static void main(final String[] args) {
+        // Setup logger
+        final Handler handler = LOGGER.getHandlers()[0];
+        handler.setFormatter(new Formatter() {
+            private final SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+
+            @Override
+            public String format(final LogRecord record) {
+                final long threadId = record.getLongThreadID();
+                final String threadName = Thread.getAllStackTraces().keySet().stream()
+                        .filter(t -> t.getId() == threadId)
+                        .findFirst()
+                        .map(Thread::getName)
+                        .orElseGet(() -> "Thread " + threadId);
+
+                return "[" + this.format.format(new Date(record.getMillis())) + "] [" + record.getLevel().getName()
+                        + "] [" + threadName + "] " + record.getMessage() + "\n";
+            }
+        });
+
+        // Set thread name, print our brand
+        Thread.currentThread().setName("Server");
+        printBrand();
+
+        // Create the event bus and add important listeners
+        final EventBus eventBus = new EventBus();
+        eventBus.registerListener(new LoginListener(eventBus));
+
+        // Create the player session controller
+        final PlayerSessionController sessionController = new PlayerSessionController(eventBus);
+
+        LOGGER.info("Starting Netty server");
+
+        // Boot up Netty
+        final NettyBootstrapper nettyBootstrapper = new NettyBootstrapper(sessionController, eventBus);
+        nettyBootstrapper.start();
+    }
+
+    private static void printBrand() {
+        LOGGER.info(" _____     _ _           ");
+        LOGGER.info("|   | |_ _| |_|_ _ _____ ");
+        LOGGER.info("| | | | | | | | | |     |");
+        LOGGER.info("|_|___|_  |_|_|___|_|_|_|");
+        LOGGER.info("      |___|              ");
+        LOGGER.info("https://github.com/cerus/nylium");
+        LOGGER.info("");
     }
 
 }
