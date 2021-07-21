d77ef83ca9de6ca21b78f6956b9e966473e3c4ee
Cerus <m.dorn.2003@web.de>
2021-07-22 01:38:46 +0200

    Add Minecraft internal plugin message keys

--- src/main/java/dev/cerus/nylium/server/key/MinecraftKeys.java
+++ src/main/java/dev/cerus/nylium/server/key/MinecraftKeys.java
@@ -2,7 +2,24 @@ package dev.cerus.nylium.server.key;
 
 public class MinecraftKeys {
 
+    // Used in plugin messages
     public static NamespacedKey BRAND = NamespacedKey.def("brand");
+    public static NamespacedKey DEBUG_PATHS = NamespacedKey.def("minecraft:debug/paths");
+    public static NamespacedKey DEBUG_NEIGHBOURS_UPDATE = NamespacedKey.def("minecraft:debug/neighbors_update");
+    public static NamespacedKey DEBUG_CAVES = NamespacedKey.def("minecraft:debug/caves");
+    public static NamespacedKey DEBUG_STRUCTURES = NamespacedKey.def("minecraft:debug/structures");
+    public static NamespacedKey DEBUG_WORLDGEN_ATTEMPT = NamespacedKey.def("minecraft:debug/worldgen_attempt");
+    public static NamespacedKey DEBUG_POI_TICKET_COUNT = NamespacedKey.def("minecraft:debug/poi_ticket_count");
+    public static NamespacedKey DEBUG_POI_ADDED = NamespacedKey.def("minecraft:debug/poi_added");
+    public static NamespacedKey DEBUG_POI_REMOVED = NamespacedKey.def("minecraft:debug/poi_removed");
+    public static NamespacedKey DEBUG_VILLAGE_SECTIONS = NamespacedKey.def("minecraft:debug/village_sections");
+    public static NamespacedKey DEBUG_GOAL_SELECTOR = NamespacedKey.def("minecraft:debug/goal_selector");
+    public static NamespacedKey DEBUG_BRAIN = NamespacedKey.def("minecraft:debug/brain");
+    public static NamespacedKey DEBUG_BEE = NamespacedKey.def("minecraft:debug/bee");
+    public static NamespacedKey DEBUG_HIVE = NamespacedKey.def("minecraft:debug/hive");
+    public static NamespacedKey DEBUG_GAME_TEST_ADD_MARKER = NamespacedKey.def("minecraft:debug/game_test_add_marker");
+    public static NamespacedKey DEBUG_GAME_TEST_CLEAR = NamespacedKey.def("minecraft:debug/game_test_clear");
+    public static NamespacedKey DEBUG_RAIDS = NamespacedKey.def("minecraft:debug/raids");
 
     private MinecraftKeys() {
     }
